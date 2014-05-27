30f4335bb7dbbda40714ee8952e2da127d961960
Aleksandr Ivanov <eealeivan@gmail.com>
2014-05-28 00:34:29 +0300

    Create self hosted NancyFx project.

---
+++ src/MixpanelDemo/Web/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Web
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => View["index.html"];
+        }
+    }
+}
